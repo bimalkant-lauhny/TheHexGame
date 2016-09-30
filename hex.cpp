5fa6a3286a57fded7955f175ae9fb4dc8eaa28cb
ferbncode <suyashgargsfam@gmail.com>
2016-09-30 12:10:30 +0530

    Added print_board

--- hex.cpp
+++ hex.cpp
@@ -8,7 +8,49 @@ struct hx
 	int minimax(int board[][m],int turn, int depth);	// minimax(board,turn,depth)
 	int evaluatescore(int board[][m],int depth); // evalutescore(board,depth)
 	int win(int board[][m],int depth); // win(board)
+
+	void print_board();
 };
+
+void hx::print_board()
+{
+		int count = 10;
+		for(int i=1;i<=n;i++)
+		{
+				for(int j=1;j<=count;j++)
+						cout << " ";
+				if(i<12)
+				{
+		           for(int j=1;j<=i;j++)
+				   {
+						   if(board[i][j] == human)
+								   cout << "B ";
+						   else if(board[i][j] == bot)
+								   cout << "R ";
+						   else
+								   cout << "0 ";
+
+				   }
+				   count-=1;
+				   if(i==11)
+						   count = 1;
+				}
+				else
+				{
+						for(int j=1;j<=(n-i);j++)
+						{
+								if(board[i][j] == human)
+								   cout << "B ";
+								else if(board[i][j] == bot)
+								   cout << "R ";
+								else
+								   cout << "0 ";
+						}
+						count ++;
+				}
+				cout << endl;
+		}
+}
 
 
 int main()
@@ -19,7 +61,7 @@ int main()
 	cout<<"Your turn(1 or 2): "; cin>>human;
 	if(human == 1) bot=-1;
 	else human = -1, bot=1;
-	
+	hex.print_board();
 	int turn = 1, xi, yj;
 	while(1)
 	{
@@ -31,10 +73,10 @@ int main()
 		{
 			cout<<"BOT: Enter cell i,j= "; cin>>xi>>yj;
 		}
-		if(xi<(n/2 +1) && yj>i ) continue;
+		if(xi<(n/2 +1) && yj>xi ) continue;
 		else if(xi >= (n/2+1) && yj>n-xi) continue;
 			
-		hex.board[x][y] = turn;
+		hex.board[xi][yj] = turn;
 		turn *= -1;
 	}
 	return 0;
