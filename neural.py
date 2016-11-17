69032e8b61ad1e11f69d25c41f1667e55936bb53
Suyash Garg <suyashgargsfam@gmail.com>
2016-11-17 15:14:28 +0530

    Neural Network Code

--- neural.py
+++ neural.py
@@ -1,29 +1,53 @@
 import numpy as np
+import pandas as pd
 
-class NeuralNet(object):
-    """Class for neural network"""
-    def __init__(self, hidden=1, output_units=2, hidden_units=3, inunits=3):
-        self.hiddenLayers = hidden
-        self.hiddenUnits = hidden_units
-        self.inputUnits = inunits
-        self.outUnits = output_units
-        self.weightsLayer1 = [[0, 0, 0],
-                              [0, 0, 0],
-                              [0, 0, 0]]
-        self.weightsLayer2 = [[0, 0, 0],
-                              [0, 0, 0]]
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
 
-    def propagate_forward(self, x):
-        # three input units 1x3 and 3x3 gives 1x3 for the hidden
-        # layer
+def sigmoidPrime(z):
+    return np.exp(-z)/((1+np.exp(-z)**2)
 
-        hidden_layer = np.dot(np.array(x), np.array(self.weightsLayer1))
-        output_units = np.dot(np.array(hidden_layer), np.array(self.weightsLayer2))
+class NeuralNetworks(object):
+    """Class for Neural Network""" 
+    def __init__(self):
 
-        return output_units
+        self.inputLayerSize = 36
+        self.hiddenLayerSize = 5
+        self.outputLayerSize = 1
+        self.alpha = 0.5
 
-    def sigmoid(z):
-        return (1/(1 + exp(-z)))
+        self.W1 = np.random.randn(self.inputLayerSize, 
+            self.hiddenLayerSize)
+        self.W2 = np.random.randn(self.hiddenLayerSize,
+            self.outputLayerSize)
+
+    def forward(self, X):
+
+        self.Z2 = np.dot(X, self.W1)
+        self.A2 = self.sigmoid(self.Z2)
+        self.A3 = np.dot(self.A2, self.W2)
+        yHat = self.sigmoid(self.Z3)
+        self.yHat = yHat
+        return yHat
 
 
+    def backward(self, X, Y):
 
+        self.yHat = self.forward(X)
+        delta_k = np.multiply(-(y - self.yHat), self.sigmoidPrime(self.z3))
+        dJdW2 = np.multiply(self.alpha,np.dot(self.A2.T, delta_k))
+
+        delta2 = np.dot(delta_k, self.W2.T)*self.sigmoidPrime(self.Z 2)
+        dJdW1 = np.multiply(self.alpha, np.dot(X.T, delta2))
+
+        self.W1 = self.W1 + dJdW1
+        self.W2 = self.W2 + dJdW2
+
+    def computeCost(self, Y):
+        return(0.5*np.sum((Y-self.yHat)**2))
+
+    def train(self, X, Y, iterations=300):
+        for iteration in iterations:
+           self.forward(X)
+           print("{}".format(self.computeCost(Y)))
+           self.backward(X, Y) 
